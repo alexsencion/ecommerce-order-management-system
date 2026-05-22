37bbfcd9a5bb29b783682f1f4fcf772594d57e5a
Alexander Sencion <alexsencion99@gmail.com>
2026-05-21 20:01:43 -0400

    feat(products): add DTOs, validators, ProductService and CategoryService

---
+++ ECommerce.Application/DTOs/Request/AdjustStockRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.DTOs.Request
+{
+    public class AdjustStockRequest
+    {
+        public int Quantity { get; set; }
+        public string Reason { get; set; } = string.Empty;
+    }
+}
---
+++ ECommerce.Application/DTOs/Request/ProductQueryParams.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.DTOs.Request
+{
+    public class ProductQueryParams
+    {
+        public string? Search { get; set; }
+        public Guid? CategoryId { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public bool? IsActive { get; set; }
+        public bool? InStockOnly { get; set; }
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 12;
+        public int ClampedPageSize => Math.Min(Math.Max(PageSize, 1), 50);
+    }
+}
